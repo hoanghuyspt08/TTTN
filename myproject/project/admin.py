c0219c0dda76d0c2398f06e615ffec77be786a4b
Hoang Huy <hoanghuyspt08@gmail.com>
2025-06-10 16:38:00 +0700

    update

--- myproject/project/admin.py
+++ myproject/project/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from models import Profile
+from .models import Profile, Hotel, Room
 
 class Profile_unit(admin.ModelAdmin):
     list_display = ('name', 'email','phone', 'birth_date','gender')
 
+class Hotel_unit(admin.ModelAdmin):
+    list_display = ('name','cost', 'address','utilities', 'describe')
+
+class Room_unit(admin.ModelAdmin):
+    list_display = ('type', 'width', 'cost', 'utilities')
+
 admin.site.register(Profile, Profile_unit)
+admin.site.register(Hotel, Hotel_unit)
+admin.site.register(Room, Room_unit)
